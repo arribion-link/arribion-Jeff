930c3a71b7fbc6c6549214ea3e2c6a1f1a0b2652
Dev Jeff <arribion@gmail.com>
2025-12-03 23:21:20 +0300

    added cursor animation

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.tsx'
 
+import TargetCursor from "./components/TargetCursor"
+
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+      <BrowserRouter>
+      <TargetCursor 
+        spinDuration={2}
+        hideDefaultCursor={true}
+        parallaxOn={true}
+      />
     <App />
     </BrowserRouter>
   </StrictMode>,
